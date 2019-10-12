82ac3e8c4485e56df3d323ee1413a676cc936a2d
Grin <grinefimov@yandex.ru>
2019-10-12 20:45:18 +0300

    Add search input validation
    * Add search input verification
    * Add search on Enter key
    * Add bundling and minification

---
+++ SearchWebApp/wwwroot/js/search.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+  document.getElementById("searchStringInput").addEventListener("keydown",
+    function (e) {
+      if (e.keyCode === 13) {
+        document.getElementById("submitButton").click();
+      }
+    });
+});
+
+function isInputNotEmpty(id) {
+  if (document.getElementById(id).value === "") {
+    return false;
+  };
+  return true;
+}
